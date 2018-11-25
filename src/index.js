394657f570be938379ea5e58e8086ab24fce781a
Ken Vermeille <ken@vermillionsky.com>
2018-11-25 16:35:20 -0500

    Add Section List

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { FlatList, View, Platform, Dimensions } from 'react-native';
+import { SectionList, View, Platform, Dimensions } from 'react-native';
 import debounce from 'lodash/debounce';
 import isEqual from 'lodash/isEqual';
 
@@ -70,16 +70,21 @@ export default class AlphaScrollFlatList extends Component {
         if (letter === '#') {
             //it's a number or a symbol, scroll to the top or to the bottom of the list
             const firstIndex = 0;
-            const lastIndex = this.props.data.length - 1;
+            const lastIndex = this.props.sections.length - 1;
 
             index = this.props.reverse ? lastIndex : firstIndex;
         } else {
             //Get index of item with that letter and scroll to the first result on the list
-            index = this.props.data.findIndex(item => item[this.props.scrollKey].charAt(0).localeCompare(letter) === 0);    
+            index = this.props.sections.findIndex(item => item[this.props.scrollKey].charAt(0).localeCompare(letter) === 0);    
         }
 
-        if (index !== -1)
-            this.list.scrollToOffset({animated: false, offset: index * this.props.itemHeight});
+        if (index !== -1) {
+            this.list.scrollToLocation({
+                animated: false,
+                sectionIndex: index,
+                itemIndex: 0
+            })
+        }
     }
 
     handleOnScrollEnds () {
@@ -90,15 +95,15 @@ export default class AlphaScrollFlatList extends Component {
         });
     }
 
-    getItemLayout (data, index) {
-        const {itemHeight} = this.props;
+    // getItemLayout (data, index) {
+    //     const {itemHeight, sectionHeight} = this.props;
         
-        return {
-            length: itemHeight, 
-            offset: itemHeight * index, 
-            index
-        };
-    }
+    //     return {
+    //         length: itemHeight, 
+    //         offset: itemHeight + sectionHeight * index,
+    //         index
+    //     };
+    // }
 
     isPortrait () {
         const {width, height} = Dimensions.get('window');
@@ -118,7 +123,7 @@ export default class AlphaScrollFlatList extends Component {
     render() {
         return (
             <View onLayout={this.handleOnLayout.bind(this)}>
-                <FlatList 
+                <SectionList 
                     {...this.props}
                     ref={elem => this.list = elem}
                 />
